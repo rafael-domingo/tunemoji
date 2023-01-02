ee6a778881cdf974583782e7f0a4bcd91d75adb1
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2023-01-02 14:16:55 -0600

    Set up Sign-in and game config front-end

---
+++ tunemoji/src/pages/loading.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Loading({ access_token, refresh_token, playlists, setPlaylists }) {
+    const navigate = useNavigate()
+
+    useEffect(() => {   
+        handleGetPlaylist('On Repeat').then(playlist => {            
+            handleGetPlaylistTracks(playlist.playlists.items[0].id)
+                .then(playlistTracks => {
+                    const playlistObject = {
+                        playlist: playlist.playlists.items[0],
+                        playlistTracks: playlistTracks.items
+                    
+                    }
+                    setPlaylists(playlists => [...playlists, playlistObject])
+                      
+            })
+        })
+        handleGetPlaylist('Your Top Songs 2022').then(playlist => {            
+            handleGetPlaylistTracks(playlist.playlists.items[0].id)
+                .then(playlistTracks => {
+                     const playlistObject = {
+                        playlist: playlist.playlists.items[0],
+                        playlistTracks: playlistTracks.items
+                    
+                    }
+                    setPlaylists(playlists =>[...playlists, playlistObject])
+            })
+        })
+      
+    }, [0])
+
+    useEffect(() => {
+        if (playlists.length === 2) {
+            navigate('/game')
+        }
+    }, [playlists])
+
+    // helper functions 
+    const handleGetPlaylist = async (playlistName) => {        
+        return fetch('/spotify/getPlaylists', {
+            method: 'POST',
+            body: new URLSearchParams({
+                access_token,
+                playlistName
+            })                          
+        })
+            .then(response => response.json())
+            .then(data => { return data })         
+    }
+
+    const handleGetPlaylistTracks = async (playlistId) => {
+        return fetch('/spotify/getTracks', {
+            method: 'POST',
+            body: new URLSearchParams({
+                access_token,
+                playlistId
+            })
+        })
+            .then(response => response.json())
+            .then(data => { return data })        
+    }
+
+    const handleGetEmojis = async (trackName) => {
+        
+    }
+
+    return (
+        <div className='container background'>
+            <h1>Getting your Spotify information</h1>
+        </div>
+    )
+}
+
+export default Loading;
